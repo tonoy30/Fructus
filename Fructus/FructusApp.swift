026da967c5f09e671e0b2aefbf1b50754fa04ac9
tonoy30 <tonoy.sust@gmail.com>
2021-11-28 01:37:02 +0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by md tonoy akanda on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
